6e6d3df3daa1584a2e54f6b341fbb9a6ea62806b
Record Man <ksj_design@naver.com>
2020-12-07 17:44:44 +0900

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,29 @@
-import React, { Component, useState, useEffect } from "react";
-import InputText from "./components/InputText";
-import Button from "./components/Button";
+import React, { useState, useEffect } from "react";
+import UserIntro from "./components/UserIntro";
+import InputForm from "./components/InputForm";
 import Screen from "./components/Screen";
+import "./App.css";
 
 function App() {
+
   // 시간 객체 생성
   const today = new Date();
-
-  const [talk, setTalk] = useState("");
+  const [message, setMessage] = useState("");
   const [contents, setContents] = useState([
     {
       item: {
+        id: "",
         date: "",
-        text: "",
+        message: "",
       },
     },
   ]);
 
-  // 초기 데이터 가져오기, 추후 비동기 처리로 바꿀지 고민
+  const sendUserId = (id: string) => {
+    localStorage.setItem("canvasUserId", id);
+  };
+
+  // 초기 데이터 가져오기
   useEffect(() => {
     setTimeout(() => {
       fetch(`/api/data/init`)
@@ -26,24 +32,37 @@ function App() {
         })
         .then((data) => {
           setContents(data);
+
+          // 초기 화면 맨 아래로 세팅
+          window.scrollTo({
+            top: document.querySelector(".screen")?.clientHeight,
+          });
         });
     }, 50);
   }, []);
 
   const changeData = (data: string) => {
-    setTalk(data);
+    setMessage(data);
   };
 
+  // 입력 데이터 전송
   const sendData = () => {
-    const time = today.toLocaleDateString() + " " + today.toLocaleTimeString();
+    if (message === "") {
+      return;
+    }
+
+    const time = today.toLocaleTimeString();
 
     // 보내질 데이터
     const sendData = {
       item: {
-        date: time,
-        text: talk,
+        id: localStorage.getItem("canvasUserId"),
+        date: time.substr(0, time.length - 3),
+        message: message,
       },
     };
+
+    setMessage("");
 
     // 전송 데이터
     console.log(sendData.item);
@@ -61,13 +80,27 @@ function App() {
       .then((data) => {
         setContents(data);
       });
+
+    screenMove();
+  };
+
+  const screenMove = () => {
+    setTimeout(() => {
+      window.scrollTo({
+        top: document.querySelector(".screen")?.clientHeight,
+        behavior: "smooth",
+      });
+    }, 50);
   };
 
   return (
     <div className="App">
-      <InputText changeData={changeData} />
-      <Button sendData={sendData} />
-      {/* {JSON.stringify(contents)} */}
+      <UserIntro sendUserId={sendUserId} />
+      <InputForm
+        message={message}
+        changeData={changeData}
+        sendData={sendData}
+      />
       <Screen contents={contents} />
     </div>
   );
